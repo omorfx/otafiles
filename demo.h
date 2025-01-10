7b42b30246d5f35e9a301d715ee0433554083f31
omorfx <omorfx@gmail.com>
2025-01-11 00:06:48 +0300

    Create demo.h

---
+++ demo.h
@@ -0,0 +1,15 @@
+#ifndef MY_LIBRARY_H
+#define MY_LIBRARY_H
+
+#include <Arduino.h>
+
+// Sabit integer tanımlaması
+const int MY_CONSTANT = 42;
+
+// Yazdırma metodu
+inline void printConstant() {
+  Serial.print("The value of MY_CONSTANT is: ");
+  Serial.println(MY_CONSTANT);
+}
+
+#endif
